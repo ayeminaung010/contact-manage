0f247524fe8d74f424d7c9881264d4189a54b323
yasuoOwO <heinmina05@gmail.com>
2022-12-16 21:35:43 +0630

    fix bugs and inprovement

--- src/components/Create.jsx
+++ src/components/Create.jsx
@@ -30,12 +30,19 @@ const Create = () => {
 
   const onSubmitHandler = e =>{
     e.preventDefault();
-    const contact = {id: Date.now(), name,email,phone}
-    apiCreateContact(contact);
-    Toast.fire({
-      icon: 'success',
-      title: 'Created successfully'
-    })
+    if(!name == "" && !email == "" && !phone == "" ){
+      const contact = {id: Date.now(), name,email,phone}
+      const status = apiCreateContact(contact);
+      Toast.fire({
+        icon: 'success',
+        title: 'Created successfully'
+      })
+    }else{
+      const errorTag = document.querySelectorAll('.error');
+      errorTag.forEach((i) => {
+        i.style.display = 'block'
+      })
+    }
   }
 
   return (
@@ -48,7 +55,9 @@ const Create = () => {
             @
           </span>
           <input type="text" onChange={(e) => setName(e.target.value)} value={name} id="website-admin" className="rounded-none rounded-r-lg bg-gray-50 border border-gray-300 text-gray-900 focus:ring-blue-500 focus:border-blue-500 block flex-1 min-w-0 w-full text-sm p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Bonnie Green" />
+          
         </div>  
+        <small className=' text-red-500 py-0 my-0 hidden error'>Required Field</small>
       </div>
       <div className="">
         <label htmlFor="website-admin" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Email</label>
@@ -58,6 +67,7 @@ const Create = () => {
           </span>
           <input type="text" onChange={(e) => setEmail(e.target.value)} value={email}  id="website-admin" className="rounded-none rounded-r-lg bg-gray-50 border border-gray-300 text-gray-900 focus:ring-blue-500 focus:border-blue-500 block flex-1 min-w-0 w-full text-sm p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="example@gmail.com" />
         </div>  
+        <small className=' text-red-500 py-0 my-0 hidden error'>Required Field</small>
       </div>
       <div className="">
         <label htmlFor="website-admin" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Username</label>
@@ -65,8 +75,9 @@ const Create = () => {
           <span className="inline-flex items-center px-3 text-sm text-gray-900 bg-gray-200 rounded-l-md border border-r-0 border-gray-300 dark:bg-gray-600 dark:text-gray-400 dark:border-gray-600">
             <BsPhone className=' text-xl text-gray-50'/>
           </span>
-          <input type="number" onChange={(e) => setPhone(e.target.value)} value={phone}  id="website-admin" className="rounded-none rounded-r-lg bg-gray-50 border border-gray-300 text-gray-900 focus:ring-blue-500 focus:border-blue-500 block flex-1 min-w-0 w-full text-sm p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="09xxxxxxx" />
+          <input type="number"  onChange={(e) => setPhone(e.target.value)} value={phone}  id="website-admin" className="rounded-none rounded-r-lg bg-gray-50 border border-gray-300 text-gray-900 focus:ring-blue-500 focus:border-blue-500 block flex-1 min-w-0 w-full text-sm p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="09xxxxxxx" />
         </div>  
+        <small className=' text-red-500 py-0 my-0 hidden error'>Required Field</small>
       </div>
       <div className="">
         <button type='submit' className=' text-white bg-gray-700 px-4 py-2 my-3 rounded-md' >
